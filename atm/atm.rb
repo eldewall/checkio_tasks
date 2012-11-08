ba6512721caec51d23726f0c7a8ac4670072818c
Erik Eldewall <eldewall@gmail.com>
2012-11-08 14:17:20 +0100

    inital commit

---
+++ atm/atm.rb
@@ -0,0 +1,25 @@
+class ATM 
+    attr_reader :balance
+
+    def initialize(balance)
+        @balance = balance
+    end
+
+    def withdraw(amount)
+        amount = Array(amount) unless amount.kind_of?(Array)
+        amount.each { |amount| draw(amount) }
+    end
+
+    def commision(amount)
+        (0.5 + (amount * 0.1)).floor
+    end
+
+ private
+    def draw(amount)
+        @balance -= (amount).floor if valid_amount?(amount) 
+    end
+
+    def valid_amount?(amount)
+        amount % 5 == 0 && (amount) < @balance
+    end
+end
---
+++ atm/atm_test.rb
@@ -0,0 +1,49 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+
+require_relative 'atm'
+
+describe ATM do
+
+    it "has a starting balance" do
+        balance = 10
+        atm = ATM.new(balance)
+        atm.balance.must_equal balance
+    end 
+
+    describe "withdraw" do
+        before do
+            @start_balance = 100
+            @atm = ATM.new(@start_balance)
+        end
+
+        it "should reject amounts not divisible with 5" do
+            @atm.withdraw(11)
+            @atm.balance.must_equal @start_balance
+        end
+
+        it "should accept amounts divisbile with 5" do
+            @atm.withdraw(10)
+            @atm.balance.must_equal @start_balance - 10
+        end
+
+        it "can not withdraw beyond current balance" do
+            current = @atm.balance
+            @atm.withdraw(current + 10)
+            @atm.balance.must_equal current
+        end
+
+        it "should handle a array of amounts" do
+            @atm.withdraw([10, 20])
+            @atm.balance.must_equal @start_balance - 30
+        end
+
+        describe "commision" do
+            
+            it "should be 0.5 minus once percent of amount" do
+                @atm.commision(10).must_equal 1
+                @atm.commision(100).must_equal (0.5 + (100 * 0.1)).floor
+            end
+        end
+    end
+end
