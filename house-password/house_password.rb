272e9b4ae1b63be870dabacb48f2ff4dd1d59827
Erik Eldewall <eldewall@gmail.com>
2012-11-09 11:43:27 +0100

    passing implementation

--- house-password/house_password.rb
+++ house-password/house_password.rb
@@ -10,13 +10,36 @@ class HousePassword
         
     def has_one_digit?
         valid = false
-        @password.chars.to_a.each { |c| valid = is_digit?(c) }
+
+        @password.chars.to_a.each do |c|
+            valid = is_digit?(c)
+            break if valid
+        end
+        
         valid
+    end
+
+    def valid?
+        has_one_upper? && has_one_lower? && has_one_digit? && valid_length?
     end
 
     def has_one_upper?
         valid = false
-        @password.chars.to_a.each { |c| valid = is_upper?(c) }
+        @password.chars.to_a.each do |c|
+            valid = !is_digit?(c) && is_upper?(c)
+            break if valid
+        end
+
+        valid
+    end
+
+    def has_one_lower?
+        valid = false
+        @password.chars.to_a.each do |c|
+            valid = !is_digit?(c) && is_lower?(c)
+            break if valid
+        end
+
         valid
     end
 
