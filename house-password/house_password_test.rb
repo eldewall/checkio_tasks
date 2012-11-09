f23504af0d170b6b8b33b7c7312ac80fcee0b0ef
Erik Eldewall <eldewall@gmail.com>
2012-11-09 10:43:44 +0100

    added house_password task

---
+++ house-password/house_password_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+
+describe HousePassword do
+
+    describe "Strong Password" do
+
+        it "has a length of 10 or greater" do
+
+        end
+
+        it "has one digit" do
+
+        end
+
+        it "has one upper case letter" do
+
+        end
+
+        it "has one lower case letter" do
+
+        end
+    end
+end
+
+
+
