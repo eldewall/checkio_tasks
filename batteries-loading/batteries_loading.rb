b644826fa4a1df1b79137927fea1100c1130992b
Erik Eldewall <eldewall@gmail.com>
2012-11-12 23:12:39 +0100

    ALMOST THERE

--- batteries-loading/batteries_loading.rb
+++ batteries-loading/batteries_loading.rb
@@ -1,12 +1,61 @@
 class BatteryLoader
-    
-    def self.balance(batteries)
+
+    def self.best_of(batteries)
         right = []
         left = []
-        batteries.sort!
+        best = batteries.inject(:+)
+
+        batteries.length.times do |i|
+            right = Array.new(batteries)
+            left = right.slice!(i, 1)
+
+            puts "l_start:" + left.to_s
+            puts "r_start:" + right.to_s
+
+            diff = BatteryLoader.diff(left, right)
+            best = diff if diff <= best
+
+            num = right.length
+
+            puts "num_times:" + num.to_s
+
+            num.times do |x|
+                left.concat(right.slice!(0, 1))
+                puts "l:" + left.to_s
+                puts "r:" + right.to_s
+
+                diff = BatteryLoader.diff(left, right)
+
+                best = diff if diff <= best
+            end
+        end
+
+        best
+    end
+
+    def self.balance(batteries)
+
+        first = self.best_of(batteries)
+        second = self.best_of(batteries.reverse())
+
+        first <= second ? first : second
+    end
+
+    def self.sum(arr)
+        if  arr.length == 1
+            arr[0]
+        elsif arr.length == 0
+            0
+        else
+            arr.inject(:+)
+        end
+
     end
 
     def self.diff(arr1, arr2)
-       
+        a1_sum = BatteryLoader.sum(arr1)
+        a2_sum = BatteryLoader.sum(arr2)
+
+        sum = (a1_sum - a2_sum).abs
     end
 end
--- batteries-loading/batteries_loading_test.rb
+++ batteries-loading/batteries_loading_test.rb
@@ -6,16 +6,53 @@ require_relative 'batteries_loading'
 describe BatteryLoader do
 
     describe "balance" do
-        it "balances equals" do
-            BatteryLoader.balance([10, 10]).must_equal 0
+
+        it "example 1" do
+            BatteryLoader.balance([10,10]).must_equal 0
+        end
+
+        it "xample 5" do
+            BatteryLoader.balance([5, 8, 13, 27, 14]).must_equal 3
         end
 
         it "example 2" do
-            BatteryLoader.balance([10]).must_equal 10
+            BatteryLoader.balance([12, 30, 30, 32, 42, 49]).must_equal 9
+        end
+ 
+        it "example 3" do
+            BatteryLoader.balance([1,1,1,3]).must_equal 0
         end
 
-        it "example 3" do
-            BatteryLoader.balance([5, 8, 13, 27, 14]).must_equal 3
+        it "example 4" do
+            BatteryLoader.balance([5,5,6,5]).must_equal 1
         end
     end
+
+
+    describe "sum" do
+
+        it "returns 0 if array is empty" do
+            BatteryLoader.sum([]).must_equal 0
+        end
+
+        it "blah" do 
+            BatteryLoader.sum([10]).must_equal 10
+        end
+    end
+
+
+
+    describe "diff" do
+        it "x " do
+            BatteryLoader.diff([10], [10]).must_equal 0
+        end
+        it "calculates difference of arrays" do
+            BatteryLoader.diff([10,20], [10,10]).must_equal 10
+        end
+
+        it "calculates right when right arg2 is larger" do
+            BatteryLoader.diff([1], [29, 30]).must_equal 58
+        end
+    end
+
 end
